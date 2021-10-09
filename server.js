7789e9f4002adaa21e508d08ca2b290222784eb9
candiceywu <candice.ying.wu@gmail.com>
2021-10-09 09:10:43 -0700

    deploying

--- server.js
+++ server.js
@@ -18,10 +18,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", {
   useNewUrlParser: true,
-  useFindAndModify: false,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
 app.use(routes);
